ab646c72bdaa2c3207cc5caf22e21bf9abe2b376
Cala 88 <mcalafell@cifpfbmoll.eu>
2020-08-12 17:26:16 +0200

    feat(App): Crear el main

---
+++ src/main/java/arteco/App.java
@@ -0,0 +1,13 @@
+package arteco;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		 System.out.println("\n" + "Cantidad de frases, palabras y parrafos del texto" + "\n" + 
+                 "==============================================="        );
+	}
+
+}
