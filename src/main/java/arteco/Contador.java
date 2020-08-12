6ee26ba1f91c32e22528f56f9b66817e1736d405
Cala 88 <mcalafell@cifpfbmoll.eu>
2020-08-12 22:08:57 +0200

    feat(Contador): Metodo devolver palabras del texto

--- src/main/java/arteco/Contador.java
+++ src/main/java/arteco/Contador.java
@@ -1,11 +1,29 @@
 package arteco;
 
-public class Contador implements Texto {
+public class Contador implements Texto{
 
 	
-	public void text(Lipsum enunciado) {
+	public int palabra(String palabras) {
+		int contador = 1, posicion;
+	    palabras= palabras.trim();                               
+	    if (palabras.isEmpty()) { 
+	        contador = 0;
+	    } else {
+	            posicion = palabras.indexOf(" "); 
+	            while (posicion != -1) {   
+	                   contador++;    
+	                   posicion = palabras.indexOf(" ", posicion + 1); 
+	            }                                     
+	    }
+	    return contador;
 		
-		
+	}
+
+	@Override
+	public String text(Lipsum enunciado) {
+		// TODO Auto-generated method stub
+		String palabras = enunciado.getLipsum();
+		return palabras;
 	}
 
 }
