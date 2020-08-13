003a11b80df581a4eea668bc4c40e058c6c6580e
Cala 88 <mcalafell@cifpfbmoll.eu>
2020-08-13 09:17:04 +0200

    feat(App): Mostrar cantidad de frases y parrafos

--- src/main/java/arteco/App.java
+++ src/main/java/arteco/App.java
@@ -13,9 +13,11 @@ public class App {
 		 
 		 Lipsum lip = new Lipsum();
 		 Contador contar = new Contador();
-		 String palabras = lip.getLipsum();
+		 String texto = lip.getLipsum();
 		 
-		 System.out.println("\n Cantidad de palabras: \n" +  contar.palabra(palabras));
+		 System.out.println("\n Cantidad de frases: \n " +  contar.frases(texto) + " frases");
+		 System.out.println("\n Cantidad de palabras: \n " +  contar.palabras(texto) + " palabras");
+		 System.out.println("\n Cantidad de parrafos: \n " +  contar.parrafos(texto) + " parrafos");
 	}
 
 }
